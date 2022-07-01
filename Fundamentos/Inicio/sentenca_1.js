ffef4b19f5ce171b6d1e0dedb5b40277d33124c2
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2022-06-30 22:26:40 -0300

    Mais sentenças e comentarios
    Um pouco mais sobre o básico de JS

--- Fundamentos/Inicio/sentenca_1.js
+++ Fundamentos/Inicio/sentenca_1.js
@@ -1,1 +1,3 @@
-console.log("Olá mundo!")
+console.log("Bom dia!")
+console.log("Boa tarde!")
+console.log("Boa noite!")
--- Fundamentos/Inicio/sentenca_2.js
+++ Fundamentos/Inicio/sentenca_2.js
@@ -1,1 +1,5 @@
-console.log("Olá mundo!")
+/* Introdução do comentário */
+//Diferentes formas de comentários
+console.log("Passo #01")
+console.log("Passo #02")
+console.log("Passo #03")
