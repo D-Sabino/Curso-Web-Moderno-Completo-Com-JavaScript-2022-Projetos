3be1edca70a0e295c8742a4e31aa835562fdd056
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2022-07-01 07:40:52 -0300

    Aula sobre os tipos de comentarios

---
+++ Fundamentos/Inicio/Comentario.js
@@ -0,0 +1,9 @@
+/* Introdução do comentário */
+//Diferentes formas de comentários
+console.log("Passo #01") //Diferentes formas de comentários
+//console.log("Passo #02")
+/*
+    Comentario
+    de multiplas
+    linhas
+*/
