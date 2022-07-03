f62d53a2ba756e64809743de01ea62509610f408
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2022-07-02 21:00:05 -0300

    "Desafio" inicial, blocos e senteças
    Porque estou realizando estas atividades? Tambem me pergunto, talvez eu deveria pular para as aulas mais avançadas, mas meu toc não deixa...
    Em compensação estou assistindo as aulas em 2x;

---
+++ Fundamentos/Inicio/desafio_sentenca_bloco.js
@@ -0,0 +1,19 @@
+//Bloco 01
+/* Imprimir números impares */
+{
+    console.log("1")
+    console.log("3")
+    console.log("5")
+    console.log("7")
+    console.log("9")
+}
+
+//Bloco 02
+/* Imprimir números pares */
+{
+    console.log("2")
+    console.log("4")
+    console.log("6")
+    console.log("8")
+    console.log("10")
+}
