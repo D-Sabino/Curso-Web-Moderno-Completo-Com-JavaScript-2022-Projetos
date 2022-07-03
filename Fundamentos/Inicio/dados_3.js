370c515d5c198cadb180f6b4b98db860985d5b94
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2022-07-03 09:27:26 -0300

    Dados 03
    Tipos de dados (básicos)

---
+++ Fundamentos/Inicio/dados_3.js
@@ -0,0 +1,14 @@
+//Number
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+let salario = 4517.32;
+console.log(typeof salario);
+
+//Boolean
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+//String
+console.log(typeof "Teste");
+console.log(typeof 'Teste');
