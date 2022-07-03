eeac8c64ef82a0bd0502144b55ceb5540b4a649a
Daniel Sabino <40477679+D-Sabino@users.noreply.github.com>
2022-07-03 09:37:20 -0300

    Desafio Area
    Calculo da área de circunferência

---
+++ Fundamentos/Inicio/desafio_area.js
@@ -0,0 +1,12 @@
+// PI = 3.14...
+// PI * raio * raio
+
+const PI = 3.14159265358979323846;
+let raio = 10;
+let areaCirc;
+
+areaCirc = PI * raio * raio;
+console.log("O valor da area e " + areaCirc);
+
+areaCirc = Math.PI * raio * raio;
+console.log("O valor da area e " + areaCirc);
